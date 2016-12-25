856767a1c603caee2c007b55c003d41950396cea
lakehanne <patlekano@gmail.com>
2016-12-25 13:10:22 -0600

    fixed casts to int with unsigned long int

--- src/atoi.cxx
+++ src/atoi.cxx
@@ -46,7 +46,7 @@ int main(int argc, char* argv[])
     std::cout << "length of input is: " << strlen(input) + 1 << std::endl;
     ulint res = do_atoi(input);
 
-    std::cout << "integer value is: " << res << std::endl;
+    std::cout << "value is: " << res << std::endl;
 
     return 0;
 }
