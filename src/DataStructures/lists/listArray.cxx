3a5d2dd1c405d4613d40d1814b0d8e3ead273eff
lakehanne <patlekano@gmail.com>
2017-01-05 17:33:28 -0600

    listarray insert problem

--- src/DataStructures/lists/listArray.cxx
+++ src/DataStructures/lists/listArray.cxx
@@ -19,131 +19,156 @@
 
 #include <cassert>
 #include <iostream>
+#include <algorithm>
+
+#define OUT(__x__)	std::cout << __x__ << std::endl;
 
 //List implementation
 template<typename T>
 class arrayList 
 {
 private:
-	//a dynamic arrayList wouldn't need maxSize
-	unsigned maxSize;			//maximum list size
-	unsigned listSize;			//current list size now
-	unsigned curPos;				//position of current element
-	T*	listArray;			//array holding list elements
+	//a dynamic arrayList wouldn't need sizeLimit
+	int sizeLimit;				//maximum list size
+	int currentSize;			//current list size now
+	int currentPos;				//position of current element
+	T*	listArray;					//array holding list elements
+
+	void clear()
+	{
+		delete [] listArray;
+		currentSize = currentPos = 0;
+		listArray = new  T[sizeLimit];
+	}
+
+	/*resize should cost O(2n) due to the doubling*/
+	inline void resize(){
+		sizeLimit *= 2;
+		int tempCurPos = currentPos;  			//keep track of current position
+		int tempListSize = currentSize;		//keep track of current currentSize
+		T* tempListArray = new T[sizeLimit];		//allocate new memory
+		std::copy(listArray, listArray+sizeLimit, tempListArray);	//copy all contents to temp listArray
+		this->clear();							//reset previous elements	
+		listArray = new T[sizeLimit];				//allocate new array listArray
+		currentSize = tempListSize;				//assign the current size of the list
+		currentPos = tempCurPos;					//reassign currentPos
+		setPos(currentPos);							//reset new position
+		delete [] tempListArray;				//delete newly allocated array memory
+		OUT("array size now: " << sizeLimit << "current Size: " << currentSize);
+	}
+protected:
+	/*these functions are not used in the program*/
+	inline void begin()
+	{
+		currentPos = 0;
+	}
+
+	inline void end()
+	{
+		currentPos = currentSize;
+	}
+
+	inline void prev()	{
+		if(currentPos !=0 )
+		{
+			--currentPos;
+		}
+	}
+
+	inline void next()	{
+		// if(currentPos != currentSize )
+			++currentPos;
+	}
+
+	const T& getCurElem() const
+	{
+		return listArray[currentPos];
+	}
 
 public:
-	arrayList(unsigned defaultSize=5000) 
-	: maxSize(defaultSize), curPos(0), listSize(curPos), listArray(new T[maxSize])
-	{}
+	arrayList(int defaultSize=5) 
+	: sizeLimit(defaultSize), currentPos(0), currentSize(0), listArray(new T[sizeLimit])
+	{
+		OUT("currentSize: " << currentSize << " maxSize: " << sizeLimit);
+	}
 	//destructor
 	~arrayList()
 	{ 
 		delete [] listArray;
 	}
 
-	void clear()
-	{
-		delete [] listArray;
-		listSize = curPos = 0;
-		listArray = new  T[maxSize];
-	}
+	arrayList(arrayList const&)=delete;			//prevent compiler copy constructor
+	arrayList& operator=(arrayList const&) = delete;		//prevent assigment copies
 
 	void insert(const T& elem)
 	{
-		if(listSize > maxSize)
-			std::cerr << "list capacity exceeded" << std::endl;
-
-		for(auto i = listSize; i > curPos; --i)
-		{
-			listArray[i] = listArray[i-1];    //shift elements up to make room
-			listArray[curPos] = elem;
-			++listSize;
-		}
+		/*We'll copy all the elements of the old array into a new array and insert*/
+		OUT("currentSize: " << currentSize << " sizeLimit: " << sizeLimit);
+		// if(currentSize < sizeLimit)	{
+			for(int i = currentSize; i < currentPos; i--)
+			{
+				listArray[i] = listArray[i-1];    //shift elements up to make room
+				OUT("listArray[i]: " << listArray[i]);
+			}
+				OUT("here");
+		// }
+		listArray[currentPos] = elem;
+		++currentSize;
+		OUT("inserted: " << elem << " into array: | size: " << size());
 	}
 
 	void append(const T& elem)
 	{
-		listArray[listSize++] = elem;
+		listArray[currentSize++] = elem;
 	}
 
 	T remove()
 	{
-		assert(curPos >= 0 && curPos < listSize);
-		T elem = listArray[curPos]; 	//copy the element
-		for (int i = curPos; i < listSize-1; ++i)
+		assert(currentPos >= 0 && currentPos < currentSize);
+		T elem = listArray[currentPos]; 	//copy the element
+		for (auto i = currentPos; i < currentSize-1; ++i)
 		{
 			listArray[i] = listArray[i+1];
 		}
-		--listSize;
+		--currentSize;
 		return elem;
 	}
 
-	void begin()
-	{
-		curPos = 0;
-	}
-
-	void end()
-	{
-		curPos = listSize;
-	}
-
-	void prev()
-	{
-		if(curPos !=0 )
-		{
-			--curPos;
+	void print() const{
+		OUT("\nprinting array elements");
+		for(auto i = 0; i < currentSize; ++i){
+			OUT(listArray[i] << " ");
 		}
-	}
-
-	void next()
-	{
-		if(curPos != listSize )
-			++curPos;
 	}
 
 	int size() const
 	{
-		return listSize;
-	}
-
-	int currPos() const
-	{
-		return curPos;
+		return currentSize;
 	}
 
 	void setPos(int pos)
 	{
-		if(curPos<0 || curPos > listSize)
-			std::cerr << "the index you have specified is out of range " << std::endl;
-		curPos = pos;
-	}
-
-	const T& getCurElem()
-	{
-		return listArray[curPos];
+		if(currentPos > currentSize)
+			resize();
+		currentPos = pos;
 	}
 };
 
 int main(void)
 {
-	arrayList<int> limp(500);
-	limp.setPos(2);
-	std::cout << "current position: " << limp.currPos() <<"\n"
-			  << "size: " << limp.size()<< std::endl;
+	arrayList<int> arrayObj(10);
 
-	limp.end();
-	std::cout << "current position after moveToEnd: " << limp.currPos() <<"\n";
+	int num;
 
-	limp.begin();
-	std::cout << "current position after moveToStart: " << limp.currPos() <<"\n";
-/*
-	for(auto el : limp)
-		std::cout << el << " " ;
+	OUT("Please enter a number to insert into the list.\n Type \"-0\" to stop.");
+	for(;;){
+		std::cin>>num;
+		if(num==-0) break;
+		arrayObj.insert(num);
+	}
 
-*/	const int i = 5;
-	limp.insert(i);
-	std::cout << "insert 50" << limp.getCurElem() << "\n";
+	// arrayObj.setPos(20);
+	arrayObj.print();
 
 
 	return 0;
